dc1e07f82fa39b3e2c426c5cac52f4c81373b880
Pc <kristaptravinsshop@aol.com>
2022-02-03 22:12:27 +0200

    footer section concept

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,23 @@
 import '../css/footer.css'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import CoffeeIcon from '@mui/icons-material/Coffee';
 
 function Contact() {
   return (
     <footer>
-        <h5>CONTACTS</h5>
-        <div className='contacts_box'>
-            
-        </div>
+       <h5> Hey, let’s keep in touch</h5>
+       <div className='email_btn'>
+           <div className='inner_btn_email'>
+            <MailOutlineIcon />
+            <p>Email</p>
+           </div>
+       </div>
+       <div className='email_btn'>
+           <div className='inner_btn_email'>
+            <CoffeeIcon />
+            <p>Buy me a coffe</p>
+           </div>
+       </div>
     </footer>
   );
 }
